2c86d7ad92668dbdb04a7e83c8700d9e2bc832fb
Gustavo <gustavo.prog40@gmail.com>
2021-06-23 19:36:31 -0300

    ee

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
     </BrowserRouter>
   )
 }
