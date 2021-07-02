a655840538ca9fb4d01d59020df520c136fd01d6
Gustavo <gustavo.prog40@gmail.com>
2021-07-02 16:43:22 -0300

    final

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -69,7 +69,7 @@ function Home() {
           <form onSubmit={handleJoinRoom}>
             <input
               type="text"
-              placeholder="Digite o Códio da Sala"
+              placeholder="Digite o Nome da Sala"
               onChange={(event) => setRoomCode(event.target.value)}
               value={roomCode}
             />
